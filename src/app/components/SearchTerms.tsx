ac63e0bc85fa87f0ee9b39fdaee1ceacbdf773be
Leonid Molochniy <leonid.molochniy@gmail.com>
2024-05-05 21:21:12 +0200

    Fixed border for the input element

--- src/app/components/SearchTerms.tsx
+++ src/app/components/SearchTerms.tsx
@@ -2,7 +2,14 @@
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { ServerData } from "../StubData";
-import { RedirectType, redirect } from 'next/navigation';
+import { ListSubheader, Paper, PaperOwnProps } from '@mui/material';
+import { CommonProps } from '@mui/material/OverridableComponent';
+import { JSX, ElementType } from 'react';
+
+type Option = {
+    title: string;
+    value: string;
+};
 
 export default function SearchTerms() {
     const result: any = []
@@ -15,13 +22,25 @@ export default function SearchTerms() {
         })
     })
 
-    type Option = {
-        title: string;
-        value: string;
-    };
     const searchTerm = (term: Option) => {
         window.location.href = `https://4urspace.com/search/${term.title}/${term.value}`;
     }
+
+    const CustomPaper = (props) => (
+        <Paper
+            {...props}
+            style={{
+                ...props.style,
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                width: 600,
+                left: '50%',
+                transform: 'translateX(-50%)',
+                position: 'absolute',
+                fontFamily: '"Open Sans", Arial, Helvetica, sans-serif'
+            }}
+        />
+    );
 
     return (
         <Autocomplete
@@ -31,8 +50,32 @@ export default function SearchTerms() {
             options={result}
             groupBy={(option: any) => option.title}
             getOptionLabel={(option: any) => option.value}
+            PaperComponent={CustomPaper}
+            renderGroup={(params) => (
+                <ul key={params.key}>
+                    <ListSubheader style={{ backgroundColor: '#E6E6E6' }}>{params.group}</ListSubheader>
+                    {params.children}
+                </ul>
+            )}
             sx={{ width: "auto", left: "100%" }}
-            renderInput={(params) => <TextField {...params} InputProps={{ ...params.InputProps, style: { height: 48, padding: "0 0 0 40px" } }} placeholder="Country, state, city, or mall" />}
+            renderInput={(params) => <TextField {...params} sx={{
+                '& .MuiOutlinedInput-root': {
+                    '.MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'transparent',
+                        borderRadius: '0px'
+                    },
+                    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'transparent',
+                        borderRadius: '0px'
+                    }
+                }
+            }}
+                InputProps={{
+                    ...params.InputProps, style: {
+                        height: 48, padding: "0 0 0 40px"
+                    }
+                }} placeholder="Country, state, city, or mall"
+            />}
         />
 
     );
